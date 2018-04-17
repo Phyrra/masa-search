28036f3cf34677124a6a695e83e6535b4e793b07
Cabadath <>
2018-04-17 22:15:46 +0200

    Remove helper function from standard module

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const { guid } = require('./dist/helpers/guid');
 const { Search } = require('./dist/Search');
 const { Type } = require('./dist/types/Type.enum');
 const { Match } = require('./dist/types/Match.enum');
 
 module.exports = {
-	guid: guid,
 	Search: Search,
 	Type: Type,
 	Match: Match
