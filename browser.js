c48216d002de67bb4c51949a4b4256c52304494c
Cabadath <masa@nbagmbp042.local>
2018-08-09 19:25:22 +0200

    Use browserify to turn into browser usable code

--- browser.js
+++ browser.js
@@ -2,8 +2,6 @@ const { Search } = require('./dist/Search');
 const { Type } = require('./dist/types/Type.enum');
 const { Match } = require('./dist/types/Match.enum');
 
-module.exports = {
-	Search,
-	Type,
-	Match
-};
+window.Search = Search;
+window.Type = Type;
+window.Match = Match;
